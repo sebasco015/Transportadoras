91b83e25e3bc648924690453b2da85ba014b9481
Sebastian <sebasco015@gmail.com>
2023-10-24 17:35:53 -0500

    arreglocodigo

--- nuevo.js
+++ nuevo.js
@@ -1,77 +1,45 @@
-// Importa las bibliotecas necesarias
-const express = require("express");
-const request = require("request");
-const cors = require("cors");
+const readline = require('readline');
 
-// Crea un servidor web
-const app = express();
-
-app.use(cors({
-    origin: "*",
-    allowedHeaders: ["Content-Type", "X-Requested-With"],
-  }));
-
-
-// Define la ruta para procesar los datos
-app.post("/procesar-datos", (req, res) => {
-  // Obtiene los datos del camión
-  const numeroCamion = req.body.numeroCamion;
-  if (!numeroCamion) {
-    throw new Error("El campo 'numeroCamion' no está presente");
-  }
-
-  const pesoCarroVacio = req.body.pesoCarroVacio;
-  const pesoCarroTotal = req.body.pesoCarroTotal;
-
-  // Calcula el peso exacto de la carga
-  const pesoCarga = pesoCarroTotal - pesoCarroVacio;
-
-  // Clasifica los sacos de café
-  const sacosDeCafe = [];
-  for (let i = 0; i < pesoCarga; i++) {
-    const pesoSaco = Math.floor(Math.random() * 100);
-    const categoriaSaco = pesoSaco === 70 ? "Especial" : "Defectuoso";
-    sacosDeCafe.push({
-      peso: pesoSaco,
-      categoria: categoriaSaco,
-    });
-  }
-
-  // Controla el espacio disponible en los contenedores
-  const contenedores = [];
-  for (let i = 0; i < 10; i++) {
-    const contenedor = {
-      numero: i + 1,
-      espacioDisponible: 1000,
-    };
-    contenedores.push(contenedor);
-  }
-
-  // Procesa los sacos de café
-  for (let saco of sacosDeCafe) {
-    // Encuentra el contenedor con espacio disponible
-    const contenedor = contenedores.find((c) => c.espacioDisponible > 0);
-    if (!contenedor) {
-      // Si no hay contenedores disponibles, envía un mensaje de error
-      res.status(400).send("No hay contenedores disponibles");
-      return;
-    }
-
-    // Agrega el saco al contenedor
-    contenedor.espacioDisponible -= saco.peso;
-    contenedor.sacos.push(saco);
-
-    // Si el contenedor está lleno, muestra un mensaje
-    if (contenedor.espacioDisponible === 0) {
-      res.send("Cambio de contenedor");
-    }
-  }
-
-  // Muestra el mensaje "Cambio de camión"
-  res.send("Cambio de camión");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
 
-// Inicia el servidor web
-app.listen(8080, () => {
-  console.log("Servidor web iniciado en el puerto 8080");
+let defectuosos = 0;
+let aprobados = 10;
+let camionActual = 0;
+
+function preguntarCamion( numeroCamiones) {
+  camionActual++;
+  if (camionActual <= numeroCamiones) {
+    console.log("Camion #" + camionActual);
+
+    rl.question("Ingrese peso del vehiculo (total): ", (pesoTotal) => {
+      rl.question("Ingrese peso del vehiculo vacio: ", (pesoVacio) => {
+        rl.question("Ingrese Saco totales: ", (sacosTotal) => {
+          for (let saco = 0; saco < (sacosTotal - aprobados); saco++) {
+            let pesoSaco = Math.floor(Math.random() * 81);
+
+            if (pesoSaco === 70) {
+              aprobados++;
+            } else if (pesoSaco < 70 || pesoSaco > 70) {
+              defectuosos++;
+            }
+          }
+
+          console.log("Sacos aprobados: " + aprobados);
+          console.log("Sacos defectuosos: " + defectuosos);
+          console.log("Peso a pagar al camionero es: " + (pesoTotal - pesoVacio));
+
+          preguntarCamion(numeroCamiones);
+        });
+      });
+    });
+  } else {
+    rl.close();
+  }
+}
+
+rl.question("Ingrese el numero de espera de camiones: ", (numeroCamiones) => {
+  preguntarCamion(numeroCamiones);
 });
